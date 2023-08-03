737d2d62f21e5173328c59c6a0aecebbb306cb16
“Sharjeel <sharjeelmazhar@protonmail.com>
2023-08-03 18:29:54 +0400

    Refactored the code

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,49 @@ import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final logger = Logger(
+    printer: PrettyPrinter(
+        methodCount: 2, // Number of method calls to be displayed
+        errorMethodCount: 8, // Number of method calls if stacktrace is provided
+        lineLength: 120, // Width of the output
+        colors: true, // Colorful log messages
+        printEmojis: true, // Print an emoji for each log message
+        printTime: false // Should each log print contain a timestamp
+        ),
+  );
+
+  void playNote(int noteNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$noteNumber.wav'));
+  }
+
+  Expanded buildNote({@required color, @required noteNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+            padding: EdgeInsets.zero,
+            minimumSize: const Size(50, 30),
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            alignment: Alignment.centerLeft),
+        onPressed: () {
+          playNote(noteNumber);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final logger = Logger(
-      printer: PrettyPrinter(
-          methodCount: 2, // Number of method calls to be displayed
-          errorMethodCount:
-              8, // Number of method calls if stacktrace is provided
-          lineLength: 120, // Width of the output
-          colors: true, // Colorful log messages
-          printEmojis: true, // Print an emoji for each log message
-          printTime: false // Should each log print contain a timestamp
-          ),
-    );
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -30,120 +53,13 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(50, 30),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.centerLeft),
-                  onPressed: () {
-                    logger.d('Pressed');
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note1.wav'));
-                    logger.d('Pressedd');
-                  },
-                  child: Container(
-                    color: const Color(0xFF8D51D8),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(50, 30),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.centerLeft),
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note2.wav'));
-                  },
-                  child: Container(
-                    color: const Color(0xFF527DDA),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(50, 30),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.centerLeft),
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note3.wav'));
-                  },
-                  child: Container(
-                    color: const Color(0xFFC0E2FB),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(50, 30),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.centerLeft),
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note4.wav'));
-                  },
-                  child: Container(
-                    color: const Color(0xFF80CE86),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(50, 30),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.centerLeft),
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note5.wav'));
-                  },
-                  child: Container(
-                    color: const Color(0xFFFFFE63),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(50, 30),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.centerLeft),
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note6.wav'));
-                  },
-                  child: Container(
-                    color: const Color(0xFFFF9D51),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: const Size(50, 30),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.centerLeft),
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note7.wav'));
-                  },
-                  child: Container(
-                    color: const Color(0xFFFF434B),
-                  ),
-                ),
-              ),
+              buildNote(color: const Color(0xFF8D51D8), noteNumber: 1),
+              buildNote(color: const Color(0xFF527DDA), noteNumber: 2),
+              buildNote(color: const Color(0xFFC0E2FB), noteNumber: 3),
+              buildNote(color: const Color(0xFF80CE86), noteNumber: 4),
+              buildNote(color: const Color(0xFFFFFE63), noteNumber: 5),
+              buildNote(color: const Color(0xFFFF9D51), noteNumber: 6),
+              buildNote(color: const Color(0xFFFF434B), noteNumber: 7),
             ],
           ),
         ),
