55e6af6c0e6251abd21ef3e47b3f111ff812b386
“Sharjeel <sharjeelmazhar@protonmail.com>
2023-08-03 17:51:53 +0400

    Xylophone implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,51 +11,143 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final logger = Logger(
+      printer: PrettyPrinter(
+          methodCount: 2, // Number of method calls to be displayed
+          errorMethodCount:
+              8, // Number of method calls if stacktrace is provided
+          lineLength: 120, // Width of the output
+          colors: true, // Colorful log messages
+          printEmojis: true, // Print an emoji for each log message
+          printTime: false // Should each log print contain a timestamp
+          ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: const Color(0xFF8D51D8),
-          body: SafeArea(
-            child: Column(
-              children: [
-                Expanded(
+        backgroundColor: const Color(0xFF8D51D8),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    logger.d('Pressed');
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note1.wav'));
+                    logger.d('Pressedd');
+                  },
                   child: Container(
                     color: const Color(0xFF8D51D8),
                   ),
                 ),
-                Expanded(
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
                   child: Container(
                     color: const Color(0xFF527DDA),
                   ),
                 ),
-                Expanded(
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                  },
                   child: Container(
                     color: const Color(0xFFC0E2FB),
                   ),
                 ),
-                Expanded(
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                  },
                   child: Container(
                     color: const Color(0xFF80CE86),
                   ),
                 ),
-                Expanded(
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
                   child: Container(
                     color: const Color(0xFFFFFE63),
                   ),
                 ),
-                Expanded(
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
                   child: Container(
                     color: const Color(0xFFFF9D51),
                   ),
                 ),
-                Expanded(
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(50, 30),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
                   child: Container(
                     color: const Color(0xFFFF434B),
                   ),
                 ),
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
